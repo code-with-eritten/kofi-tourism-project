01c1c077d2c8b32d6d1bb86758fbee7f446d276f
unknown <eritteng@gmail.com>
2024-09-26 16:05:25 -0700

    feat/destinations: Add the app to the installed apps

--- tour/settings.py
+++ tour/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # project apps
+    'destinations',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
