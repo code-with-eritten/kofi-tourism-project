c66a4fec793381d13ab38cd0afe7bb4088ecc6c1
unknown <eritteng@gmail.com>
2024-10-03 15:44:54 -0700

    feat/tour-operators: Improve tour operators model and Add services model

--- tour_operators/models.py
+++ tour_operators/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-from markdownx.models import MarkdownxField
-
-
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class TourOperator(models.Model):
     name = models.CharField(max_length=255)
-    description = MarkdownxField()
-    contact_info = models.TextField()
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=20, blank=True)
     website = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to='tour_operators/', blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    def str(self):
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    tour_operator = models.ForeignKey(TourOperator, related_name='services', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
         return self.name
