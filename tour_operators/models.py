2d8faaf2a24bfa8ec57155540676df82249abb9c
unknown <eritteng@gmail.com>
2024-10-03 15:22:04 -0700

    feat/tour-operators: Add tour operator model

--- tour_operators/models.py
+++ tour_operators/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TourOperator(models.Model):
+    name = models.CharField(max_length=255)
+    description = MarkdownxField()
+    contact_info = models.TextField()
+    website = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='tour_operators/', blank=True, null=True)
+
+    def str(self):
+        return self.name
